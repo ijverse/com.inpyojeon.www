3ff907df92d034d71f17ce084a172f7e18fe57e5
Inpyo Jeon <ijbatsy@gmail.com>
2020-08-30 00:27:43 +0900

    Default

--- js/common.js
+++ js/common.js
@@ -2,3 +2,17 @@ function moveTo(title){
 	var target = $('#'+title).offset().top-67;
 	window.scrollTo({top:target, behavior:'smooth'});
 }
+
+function openPopup(id){
+	$('.dim').css('display', 'block');
+	$('.detail#'+id).css('display', 'block');
+}
+
+function closePopup(){
+	$('.dim').css('display', 'none');
+	$('.detail').css('display', 'none');
+}
+
+function changeLang(lang){
+	$('body').removeClass().addClass('ver_'+lang);
+}
